ff34b90755b1ae8dd35685b9827d8ec95357126c
= <chickenfromouterspace@github.com>
2019-03-19 12:17:43 -0400

    Included lidar package

---
+++ igvc2019_lidar/nodes/detect_lidar.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Heather Connors
+# Jade Zsiros
+# EGN4060C Final Project
+# Cylinder detection by random sample vote
+
+import rospy, math, random
+import numpy as np
+import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2
+
+class Lidar:
+   
+	def __init__(self, scan_topic="velodyne_points"):
+		# Subscribe to the laser scan topic
+		self.scan_sub = rospy.Subscriber(scan_topic, PointCloud2, self.on_scan)
+
+	def on_scan(self, cloud):
+		# Take in our x, y, z, values and store them in the generator
+		self.xyz_generator = pc2.read_points(cloud, skip_nans=True, field_names=("x", "y", "z"))
+		# Create an iterable array out of the generator
+		iterable = [np.array([i[0], i[1], i[2]]) for i in self.xyz_generator]
+		# Create a numpy array out of the iterable
+		self.points = np.array(iterable)
+
+class Map:
+
+	def __init__(self):
+		self.worldmap = 0
+
+class Camera:
+
+	def __init__(self):
+		self.color = 0
+
+if __name__ == '__main__':
+	rospy.init_node("display", anonymous = False)	
+	lidar = Lidar()
+	occmap = Map()
+	rospy.spin()
