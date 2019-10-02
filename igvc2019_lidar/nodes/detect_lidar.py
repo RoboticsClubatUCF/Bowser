2cfeb7b078364513e61d47bdbfdf4fe7830b0926
Don Vo <don.quy.vo@gmail.com>
2019-10-02 10:35:15 -0400

    These are the scripts and findings related to the LiDAR calibration.

--- igvc2019_lidar/nodes/detect_lidar.py
+++ igvc2019_lidar/nodes/detect_lidar.py
@@ -8,7 +8,8 @@
 import rospy, math, random
 import numpy as np
 import sensor_msgs.point_cloud2 as pc2
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
 
 class Lidar:
    
@@ -23,6 +24,9 @@ class Lidar:
 		iterable = [np.array([i[0], i[1], i[2]]) for i in self.xyz_generator]
 		# Create a numpy array out of the iterable
 		self.points = np.array(iterable)
+		self.dist = math.sqrt(pow(self.points[0][0], 2) + pow(self.points[0][1], 2) + pow(self.points[0][2], 2))
+		if self.dist < 2.0 or self.dist > 2.5:
+			print self.points[0]
 
 class Map:
 
@@ -35,7 +39,7 @@ class Camera:
 		self.color = 0
 
 if __name__ == '__main__':
-	rospy.init_node("display", anonymous = False)	
+	rospy.init_node("display", anonymous = False)
 	lidar = Lidar()
 	occmap = Map()
 	rospy.spin()
