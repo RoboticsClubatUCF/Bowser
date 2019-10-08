07169b4d9b8d6acc1af90012ba5070f93ce5738d
darien-craig <darien@craigs.us>
2019-10-08 18:57:37 -0400

    removed build and devel and thereby repented for my sins

--- catkin_ws/build/bowser_states/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/darien/Bowser/catkin_ws/src/bowser_states
-# Build directory: /home/darien/Bowser/catkin_ws/build/bowser_states
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
