d622b267e0b58e22fba5910b6f9fc04856d560ea
Wesley Fletcher <wesfletch@gmail.com>
2020-10-09 17:51:03 -0400

    fixes hardcoded model paths

--- catkin_ws/src/bowser_sim/sim_setup.sh
+++ catkin_ws/src/bowser_sim/sim_setup.sh
@@ -12,10 +12,14 @@ then
 	sudo apt install -y ros-melodic-gazebo-ros-pkgs
 	sudo apt install -y ros-melodic-rtabmap
 	sudo apt install -y ros-melodic-navigation
+
+	# so gazebo can find our models
+	echo "source /usr/share/gazebo-9/setup.sh" >> ~/.bashrc
+
 elif [ "$ROS_DISTRO" == "noetic" ]
 then
 	# since we have noetic, install things the dumb way
-	echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+	echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 	source ~/.bashrc	
 
 	cd ~/Bowser/catkin_ws/src
@@ -25,9 +29,12 @@ then
 	sudo apt install -y ros-noetic-gazebo-ros-pkgs
 	sudo apt install -y ros-noetic-rtabmap
 	sudo apt install -y ros-noetic-navigation
+
+	# so gazebo can find our models
+	echo "source /usr/share/gazebo-11/setup.sh" >> ~/.bashrc
 fi 
 
-# source our bowser workspace every time the terminal is opened
+# source our bowser workspace every time the terminal is opened by adding source command to ~/.bashrc
 echo "source ~/Bowser/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 # setting up gazebo paths
@@ -35,9 +42,12 @@ echo "source ~/Bowser/catkin_ws/devel/setup.bash" >> ~/.bashrc
 export GAZEBO_PLUGIN_PATH=:~/Bowser/catkin_ws/src/bowser_sim/plugins/velodyne_plugin/build:~/Bowser/catkin_ws/src/bowser_sim/plugins/velo_360_plugin/build/devel/lib
 export GAZEBO_MODEL_PATH=~/Bowser/catkin_ws/src/bowser_sim/models:~/Bowser/catkin_ws/src/bowser_sim/urdf:
 
+# . is the same as source
 . ~/.bashrc
 
 # build our bowser workspace
 cd ~/Bowser/catkin_ws
 catkin_make
+
+. ~/.bashrc
 
