a7a0cc1aebbff340c8bd6e2bec96c6222d44af48
Vijay Stroup <vijaystroup@gmail.com>
2020-10-16 15:02:21 -0400

    :smiling_imp: the canny edger

--- catkin_ws/src/bowser_nav/vijay_lane_stuff/lane.py
+++ catkin_ws/src/bowser_nav/vijay_lane_stuff/lane.py
@@ -43,30 +43,45 @@ class Feed:
 
     """
 
-    BLACK = 0
-    WHITE = 255
-
     def __init__(self, image_path):
         self.image = cv2.imread(image_path)
 
     def to_grey(self):
         """Convert the image to grey scale so our color channel will only be 1
         with values ranging from 0 to 255 to make it faster and eaiser to
-        determine change in intensity of pixel values."""
+        determine change in intensity of pixel values.
+        
+        """
 
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
     def to_blur(self):
         """We use a Gaussian Blur to smoothen hard edges that could give us
         false positives on edges, but keep the very dramatic average change in
-        pixel values we need to see the lane markings."""
+        pixel values we need to see the lane markings.
+        
+        """
 
         kernal = (5, 5)
         deviation = 0
 
         self.image = cv2.GaussianBlur(self.image, kernal, deviation)
 
+    def to_canny(self):
+        """Canny determines the relative intensity in pixel values within a
+        particular region. The Canny function preforms a derivative with
+        respect to x and y - f(x, y) to determine intensity with respect to
+        adjacent pixels (the gradient).
+        Notes
+        -----
+        The underlying function cv2.Canny() preforms a Gaussian Blur.
+        
+        """
 
+        low_threshold = 50
+        high_threshold = 150
+
+        self.image = cv2.Canny(self.image, low_threshold, high_threshold)
 
     def show_image(self):
         """Show image and wait for keystroke."""
